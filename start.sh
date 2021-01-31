9985bf892b9d0eb121a3006468bdc4689f573331
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-01-31 21:36:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u Mnnhqrzc4oAn86waZyyMppG1rDhVyXy8cx.Avella
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u Mnnhqrzc4oAn86waZyyMppG1rDhVyXy8cx.Teje
 
 
 done
