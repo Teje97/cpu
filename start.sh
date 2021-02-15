902187958f782b1da0d195aef56e7eccded7e646
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-02-15 21:25:41 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DHUuSPqxuSusRQyJL1dvwLsmBVMEm7H37a -p x c=DOGE -t 4
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DHUuSPqxuSusRQyJL1dvwLsmBVMEm7H37a -p c=DOGE
 
 
 done
